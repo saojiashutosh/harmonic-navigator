0f4f7bdcc7698c31097b0feed6d29c451b88b481
ashutoshsaoji <ashutosh.saoji@atomicloops.com>
2026-03-14 21:31:02 +0530

    feat: Add authentication, exception handling, filtering, pagination, rendering, and email functionality

--- harmonic_navigator/serializers.py
+++ harmonic_navigator/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from collections import OrderedDict
 from utils.time import convert_time
 
 
@@ -16,3 +17,36 @@ class HarmonicBaseSerializer(serializers.ModelSerializer):
         timezone = self.context['request'].META.get(
             'HTTP_X_TIMEZONE_REGION', None)
         return convert_time(instance.updatedAt, timezone)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        action = self.context["view"].action
+        permission = self.context["request"].user == instance.id
+        if action == "list":
+            return OrderedDict(
+                {key: data[key]
+                    for key in data if key in self.Meta.list_fields}
+            )
+        if action == "retrieve":
+            if not permission:
+                return data
+            return OrderedDict(
+                {key: data[key] for key in data if key in self.Meta.get_fields}
+            )
+        return data
+
+    @property
+    def errors(self):
+        # get errors
+        errors = super().errors
+        verbose_errors = {}
+
+        fields = {field.name: field.verbose_name for field in
+                  self.Meta.model._meta.get_fields() if hasattr(field, 'verbose_name')}
+
+        for field_name, error in errors.items():
+            if field_name in fields:
+                verbose_errors[str(fields[field_name])] = error
+            else:
+                verbose_errors[field_name] = error
+        return verbose_errors
--- harmonic_navigator/views.py
+++ harmonic_navigator/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import Response
+from rest_framework import status
+import django
 
-# Create your views here.
+
+class HarmonicViewSet(ModelViewSet):
+    # renderer_classes = AtomicJsonRenderer
+
+    # # TODO  write queryset
+    # def get_queryset(self):
+    #     """
+    #     1. filter inactive user
+    #     2. filter level zero user
+    #     3. Comments
+    #     """
+    #     if self.request.user_level == 0:
+    #         return self.serializer_class.Meta.model.objects.all()
+    #     if self.request.user_level == 5:
+    #         return self.serializer_class.Meta.model.objects.exclude(userId__user_level=0)
+    #     return self.serializer_class.Meta.model.objects.exclude(userId__is_active=False).exclude(userId__user_level=0)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            self.perform_destroy(instance)
+        except django.db.models.deletion.ProtectedError as e:
+            return Response(status=status.HTTP_423_LOCKED, data={'detail': str(e)})
+        except django.db.models.deletion.RestrictedError as e:
+            return Response(status=status.HTTP_423_LOCKED, data={'detail': str(e)})
+        # self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
